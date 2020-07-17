c40d9bb4770ffc3cb99baf8a8eed2d265f20c1d1
Craig Jones <craig@strongdm.com>
2020-07-17 16:52:38 -0500

    2_managing_accounts: add create_user example

---
+++ 2_managing_accounts/create_user.rb
@@ -0,0 +1,39 @@
+# Copyright 2020 StrongDM Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require 'strongdm'
+
+# Load the SDM API keys from the environment.
+# If these values are not set in your environment,
+# please follow the documentation here:
+# https://www.strongdm.com/docs/admin-guide/api-credentials/
+api_access_key = ENV['SDM_API_ACCESS_KEY']
+api_secret_key = ENV['SDM_API_SECRET_KEY']
+if api_access_key.nil? || api_secret_key.nil?
+  puts 'SDM_API_ACCESS_KEY and SDM_API_SECRET_KEY must be provided'
+  return
+end
+client = SDM::Client.new(api_access_key, api_secret_key, host: 'api.strongdmdev.com:443')
+
+user = SDM::User.new(
+  email: 'example@example.com',
+  first_name: 'example',
+  last_name: 'example'
+)
+
+response = client.accounts.create(user)
+
+puts 'Successfully created user.'
+puts "  Email: #{response.account.email}"
+puts "  ID: #{response.account.id}"
